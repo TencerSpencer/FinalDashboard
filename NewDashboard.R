47348c9015994f63f5fca2d72142db049393ecb0
TencerSpencer <57374486+TencerSpencer@users.noreply.github.com>
2023-05-18 21:42:25 -0500

    christian did it

--- NewDashboard.R
+++ NewDashboard.R
@@ -23,7 +23,9 @@ library(circlepackeR)
 
 # data loading
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-finalData <- read.csv("Data/restaurantsWithRegions.csv")
+
+finalData <- read.csv("Data/restaurantDataVisProjData.csv")
+stylesData <- read.csv("Data/Food_Origins.csv")
 
 # region MAIN MAP DATA FORMATTING
 ## pointsDF: A data.frame whose first column contains longitudes and
